bd39c1171b8f2ef484f22eed273a4fb92a38b854
John Manuel Carado <carado.johnp11q@gmail.com>
2026-02-11 23:02:00 +0800

    feat: Add clock class for checking T

--- src/aevum/base.py
+++ src/aevum/base.py
@@ -14,3 +14,16 @@ class ProcessResult:
     waiting_time: int
     turnaround_time: int
     completion_time: int
+
+class Clock:
+    def __init__(self):
+        self._time = 0
+
+    @property
+    def time(self) -> int:
+        return self._time
+
+    def tick(self) -> int:
+        """Advance the system heartbeat by 1 unit."""
+        self._time += 1
+        return self._time
